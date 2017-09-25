07376c98cfda42ca4b9fee8e9c61e47e9322e908
xseignard <xavier.seignard@gmail.com>
2017-09-25 14:43:41 +0200

    REducing bundle size

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './main.css';
 
 import Routes from './routes';
-
-injectTapEventPlugin();
 
 const App = () => (
 	<MuiThemeProvider>
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const plugins = [new HtmlWebpackPlugin({ template: 'src/index.html' })];
-if (process.env.NODE_ENV === 'production') plugins.push(new webpack.optimize.UglifyJsPlugin());
+if (process.env.NODE_ENV === 'production') {
+	plugins.push(
+		new webpack.DefinePlugin({
+			// <-- key to reducing React's size
+			'process.env': {
+				NODE_ENV: JSON.stringify('production'),
+			},
+		})
+	);
+	plugins.push(new webpack.optimize.UglifyJsPlugin()); //minify everything
+	plugins.push(new webpack.optimize.AggressiveMergingPlugin()); //Merge chunks
+}
 
 module.exports = {
 	entry: './src/main.js',
